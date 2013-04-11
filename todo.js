b23179208ea0e9f16609336d26b0472e822a4eb5
piixelize <stephanie.k.liao@gmail.com>
2013-04-11 14:26:59 -0700

    functioning but ugly todo list

--- todo.js
+++ todo.js
@@ -1,35 +1,52 @@
 function ToDoList() {
-	this.items = {};
 	this.counter = 0;
 }
 
-ToDoList.prototype.add = function() {
-	var id = "item" + this.counter;
-	this.counter++;
+var counter = 0; 
+
+function add() {
+	var id = "item" + counter;
+	counter++;
 	var value = document.getElementById('newItem').value;
 
-	this.items[id] = new Item(id, value);
-	
-	var ul = document.getElementById("list");
-	var li = document.createElement("li");
-	li.id = id;
-	li.appendChild(document.createTextNode(value));
-	ul.appendChild(li);
+	createTask(id, value);
+
+	//resetting
 	document.getElementById('newItem').value = "";
 	document.getElementById('newItem').focus();
 
 }
 
-ToDoList.prototype.remove = function(id) {
-	var ul = document.getElementById("list");
-	var li = document.getElementById(id);
-	ul.removeChild(li);
-	delete this.items[id];
+function createTask(id, value) {
+	var outerdiv = document.getElementById("container");
+	var div = document.createElement("div");
+	div.id = id;
+
+	var input = document.createElement("input");
+	input.type = "checkbox";
+
+	var label = document.createElement("label");
+	label.appendChild(document.createTextNode(" " + value));
+
+	var delbutton = document.createElement("button");
+	delbutton.innerHTML = "delete";
+	delbutton.onclick = function() {deleteTask(id)};
+
+	div.appendChild(input);
+	div.appendChild(label);
+	div.appendChild(delbutton);
+	outerdiv.appendChild(div);
 }
 
 function Item(index, task) {
 	this.id = index;
 	this.content = task;
+}
+
+function deleteTask(id) {
+	var div = document.getElementById(id);
+	var outerdiv = document.getElementById("container");
+	outerdiv.removeChild(div);
 }
 
 function handleEnter(event) {
@@ -39,3 +56,4 @@ function handleEnter(event) {
 }
 
 var list = new ToDoList();
+
